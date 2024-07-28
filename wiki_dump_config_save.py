634772d2b9565b1988c16efc251f4a047ebbd91e
Jessica Woods <woodywoods4@outlook.com>
2024-07-28 13:48:40 -0400

    Update wiki_dump_config_save.py
    adjusting for memory issues in java.lang.OutOfMemoryError in Apache Spark

--- wiki_dump_config_save.py
+++ wiki_dump_config_save.py
@@ -8,8 +8,21 @@ from lxml import etree
 # using pyspark
 from pyspark.sql import SparkSession
 import logging
+from pyspark import SparkConf
 
-#configure logging
+# ran into memory issues due to the file being really big
+# this will help with memory allowcations
+# can also do this in bash commands, but my bash is already complicated enough, incase I need to adjust this its
+# best to just have it here.
+conf = SparkConf() \
+    .set("spark.sql.shuffle.partitions", "200") \
+    .set("spark.default.parallelism", "200") \
+    .set("spark.executor.memory", "4g") \
+    .set("spark.driver.memory", "4g")
+
+
+
+# configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -19,6 +32,8 @@ spark = SparkSession.builder.appName("XML Processing").config("spark.jars.packag
 
 # fingers crossed, load XML
 logger.info("Loading XML data...")
+
+# move this down to smaller sizes of chunks to do to adjust for the memory issues in java.lang.OutOfMemoryError
 xml_df = spark.read.format('xml').option("rowTag", "page").load('/home/jessica/Documents/enwiki-20240701-pages-meta-history1.xml')
 logger.info("XML data loaded successfully.")
 # use bash :spark-submit --packages com.databricks:spark-xml_2.12:0.13.0 wiki_dump_config_save.py
@@ -32,8 +47,9 @@ def parse_xml(row):
     return (title, text)
 
 # Apply parsing function to each row in DataFrame
+# want to use persist() or catch() so it helps with memory
 logger.info("Parsing XML data...")
-parsed_data = xml_df.rdd.map(parse_xml)
+parsed_data = xml_df.rdd.map(parse_xml).persist()
 
 # Example: Collect and print parsed data
 for title, text in parsed_data.collect():
