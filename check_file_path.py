8d2dec97ede6e4f614292629d3737de61f17fea4
Jessica Woods <woodywoods4@outlook.com>
2024-07-28 12:26:27 -0400

    Update check_file_path.py
    moving file on github

--- check_file_path.py
+++ check_file_path.py
@@ -1,6 +1,6 @@
 import os
 script_dir = os.path.dirname(__file__)
-directory_path =os.path.join(script_dir, 'enwiki-20240701-pages-meta-history1.xml')
+directory_path =os.path.join(script_dir, '/home/jessica/Documents/enwiki-20240701-pages-meta-history1.xml')
 print(os.getcwd())
 
 if os.path.exists(directory_path):
