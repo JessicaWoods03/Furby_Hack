e0399a5279aa1b5566c32d564591f862a77ee9ac
Jessica Woods <woodywoods4@outlook.com>
2024-10-19 13:22:52 -0400

    Update test_chromadb_install.py

--- test_chromadb_install.py
+++ test_chromadb_install.py
@@ -7,4 +7,12 @@ print(chromadb.__version__)
 
 client = Client()
 print("ChromaDB Client created successfully, in pyspark_env!")
+collection = client.create_collection("example_collection")
+documents = ["Hello World"]
+ids = ["1"]
+collection.add(documents=documents, ids=ids)
+
+# check data has been added
+data = collection.get()
+print("Data in ChromaDB:", data)
 spark.stop()
